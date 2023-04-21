b462c9210b77443a394f8ca5c032b6b1b967bcad
Luke Taylor <luke@circleleasing.co.uk>
2023-04-21 12:36:25 +0100

    Member details

--- member.php
+++ member.php
@@ -43,12 +43,16 @@ $contacts = array(
 				font-weight: bold;
 			}
 
-			.card .table{
+			.card .table {
 				margin-bottom: 0;
 			}
 
-			.card .table tr:last-child{
+			.card .table tr:last-child {
 				border-bottom-color: transparent;
+			}
+
+			.form-floating textarea.form-control {
+				height: 5rem;
 			}
 		</style>
 	</head>
@@ -95,40 +99,87 @@ $contacts = array(
 				</div>
 			</div>
 
-			<div class="row row-cols-1 row-cols-lg-2">
+			<div class="card mb-3">
+				<div class="card-header">Medical Details</div>
+				<div class="card-body">
+					<div class="form-floating mb-3">
+						<textarea class="form-control" placeholder="E.g. Asthma" id="medical">N/A</textarea>
+						<label for="medical">Medical Conditions</label>
+					</div>
+
+					<div class="form-floating mb-3">
+						<textarea class="form-control" placeholder="E.g.Nuts" id="allergies">N/A</textarea>
+						<label for="allergies">Allergies</label>
+					</div>
+
+					<div class="row row-cols-1 row-cols-md-2">
+						<div class="col">
+							<div class="form-floating mb-3">
+								<input type="date" class="form-control" id="tetanus" placeholder="Last Tetanus Jab" value="2012-01-01" required>
+								<label for="tetanus">Last Tetanus Jab</label>
+							</div>
+						</div>
+
+						<div class="col">
+							<div class="form-check form-switch">
+								<input class="form-check-input" type="checkbox" role="switch" id="wounds" checked>
+								<label class="form-check-label" for="wounds">Consent to clean/dress wounds?</label>
+							</div>
+							<div class="form-check form-switch">
+								<input class="form-check-input" type="checkbox" role="switch" id="medication">
+								<label class="form-check-label" for="medication">Consent to administer paracetamol/ibuprofen?</label>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="row row-cols-1 row-cols-md-2">
 				<?php 
 					for($i = 0; $i < count($contacts); $i++){ 
 						$contact = $contacts[$i]; 
 				?>
 				<div class="col">
 					<div class="card mb-3">
-						<div class="card-header">Contact <?php echo $i + 1 ?> </div>
-						<!-- <div class="card-body"> -->
+						<div class="card-header d-flex align-items-center">Contact
+							<?php echo $i + 1 ?> <button class="btn btn-primary ms-auto">Edit</button>
+						</div>
 						<table class="table table-hover details">
 							<tbody>
 								<tr>
 									<td>Name</td>
-									<td><?php echo $contact['name'] ?></td>
+									<td>
+										<?php echo $contact['name'] ?>
+									</td>
 								</tr>
 								<tr>
 									<td>Relationship</td>
-									<td><?php echo $contact['relationship'] ?></td>
+									<td>
+										<?php echo $contact['relationship'] ?>
+									</td>
 								</tr>
 								<tr>
 									<td>Mobile</td>
-									<td><?php echo $contact['mobile'] ?></td>
+									<td>
+										<?php echo $contact['mobile'] ?>
+									</td>
 								</tr>
 								<tr>
 									<td>Landline</td>
-									<td><?php echo $contact['landline'] ?></td>
+									<td>
+										<?php echo $contact['landline'] ?>
+									</td>
 								</tr>
 								<tr>
 									<td>Email</td>
-									<td><a href="mailto:<?php echo $contact['email'] ?>"><?php echo $contact['email'] ?></a></td>
+									<td><a href="mailto:<?php echo $contact['email'] ?>">
+											<?php echo $contact['email'] ?>
+										</a></td>
 								</tr>
 								<tr>
 									<td>Address</td>
-									<td><?php echo $contact['address']['line1'] ?><br />
+									<td>
+										<?php echo $contact['address']['line1'] ?><br />
 										<?php echo $contact['address']['line2'] ?><br />
 										<?php echo $contact['address']['city'] ?><br />
 										<?php echo $contact['address']['postcode'] ?>
@@ -136,10 +187,37 @@ $contacts = array(
 								</tr>
 							</tbody>
 						</table>
-						<!-- </div> -->
 					</div>
 				</div>
 				<?php } ?>
+
+				<div class="col">
+					<div class="card">
+						<div class="card-header d-flex align-items-center">Doctor <button class="btn btn-primary ms-auto">Edit</button></div>
+						<table class="table table-hover details">
+							<tbody>
+								<tr>
+									<td>Name</td>
+									<td>
+										<?php echo $contact['name'] ?>
+									</td>
+								</tr>
+								<tr>
+									<td>Surgery</td>
+									<td>
+										<?php echo $contact['relationship'] ?>
+									</td>
+								</tr>
+								<tr>
+									<td>Phone</td>
+									<td>
+										<?php echo $contact['mobile'] ?>
+									</td>
+								</tr>
+							</tbody>
+						</table>
+					</div>
+				</div>
 			</div>
 		</div>
 	</body>
