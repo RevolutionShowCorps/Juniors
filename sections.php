cd35c86dea371d65e6be23b69f18feb2a8982b7a
Luke Taylor <luke@circleleasing.co.uk>
2023-04-21 14:59:13 +0100

    Add edit/view

--- sections.php
+++ sections.php
@@ -15,12 +15,12 @@ require_once('head.php');
 						<div class="card mb-3">
 							<div class="card-header button-header">Members <button class="btn btn-primary">Add</button></div>
 							<div class="list-group list-group-flush">
-								<a href="member.php" class="list-group-item list-group-item-action">Jacob Gill</a>
-								<a href="member.php" class="list-group-item list-group-item-action">Oliver Archaki</a>
-								<a href="member.php" class="list-group-item list-group-item-action">Albie Jaques</a>
-								<a href="member.php" class="list-group-item list-group-item-action">Sofia Cumbo</a>
-								<a href="member.php" class="list-group-item list-group-item-action">Evangeline Pedder-Stratton</a>
-								<a href="member.php" class="list-group-item list-group-item-action">Preeyan Mistry</a>
+								<div class="list-group-item">Jacob Gill <span class="hover float-end">Edit | <a href="member.php">View</a></span></div>
+								<div class="list-group-item">Oliver Archaki <span class="hover float-end">Edit | <a href="member.php">View</a></span></div>
+								<div class="list-group-item">Albie Jaques <span class="hover float-end">Edit | <a href="member.php">View</a></span></div>
+								<div class="list-group-item">Sofia Cumbo <span class="hover float-end">Edit | <a href="member.php">View</a></span></div>
+								<div class="list-group-item">Evangeline Pedder-Stratton <span class="hover float-end">Edit | <a href="member.php">View</a></span></div>
+								<div class="list-group-item">Preeyan Mistry <span class="hover float-end">Edit | <a href="member.php">View</a></span></div>
 							</div>
 						</div>
 					</div>
@@ -29,12 +29,12 @@ require_once('head.php');
 						<div class="card mb-3">
 							<div class="card-header button-header">Instructors <button class="btn btn-primary">Add</button></div>
 							<div class="list-group list-group-flush">
-								<a href="member.php" class="list-group-item list-group-item-action">Sam Martin <span class="badge text-bg-primary">Caption Head</span></a>
-								<a href="member.php" class="list-group-item list-group-item-action">Megan Mouncey</a>
-								<a href="member.php" class="list-group-item list-group-item-action">Phillip Sorrenson</a>
-								<a href="member.php" class="list-group-item list-group-item-action">Megan Spencer</a>
-								<a href="member.php" class="list-group-item list-group-item-action">Oliver Richardson</a>
-								<a href="member.php" class="list-group-item list-group-item-action">Mike Seymour</a>
+								<div class="list-group-item">Sam Martin <span class="badge text-bg-primary">Caption Head</span> <span class="hover float-end">Edit | <a href="member.php">View</a></span></div>
+								<div class="list-group-item">Megan Mouncey <span class="hover float-end">Edit | <a href="member.php">View</a></span></div>
+								<div class="list-group-item">Phillip Sorrenson <span class="hover float-end">Edit | <a href="member.php">View</a></span></div>
+								<div class="list-group-item">Megan Spencer <span class="hover float-end">Edit | <a href="member.php">View</a></span></div>
+								<div class="list-group-item">Oliver Richardson <span class="hover float-end">Edit | <a href="member.php">View</a></span></div>
+								<div class="list-group-item">Mike Seymour <span class="hover float-end">Edit | <a href="member.php">View</a></span></div>
 							</div>
 						</div>
 					</div>
@@ -43,3 +43,5 @@ require_once('head.php');
 		</div>
 	</div>
 </body>
+
+</html>
