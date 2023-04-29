737e827aa0ff0157cafecdf56e581189aa1d56b1
Luke Taylor <luke@circleleasing.co.uk>
2023-04-29 16:27:19 +0100

    Create contact

--- member.php
+++ member.php
@@ -5,17 +5,30 @@ require_once("DB.php");
 
 $validationErrors = array();
 
-function validateData(){
-	global $validationErrors;
+function get_guid() {
+    $data = PHP_MAJOR_VERSION < 7 ? openssl_random_pseudo_bytes(16) : random_bytes(16);
+    $data[6] = chr(ord($data[6]) & 0x0f | 0x40);    // Set version to 0100
+    $data[8] = chr(ord($data[8]) & 0x3f | 0x80);    // Set bits 6-7 to 10
+    return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
+}
 
-	$requiredFields = array("fname", "lname", "gender", "dob");
-	$dates = array("dob", "tetanus");
+function checkRequiredFields($fields, $array){
+	$validationErrors = array();
 
-	foreach($requiredFields as $field){
-		if(!isset($_POST[$field]) || trim($_POST[$field]) == ""){
+	foreach($fields as $field){
+		if(!isset($array[$field]) || trim($array[$field]) == ""){
 			$validationErrors[$field] = "This field is required";
 		}
 	}
+
+	return $validationErrors;
+}
+
+function validateData(){
+	global $validationErrors;
+	$dates = array("dob", "tetanus");
+
+	$validationErrors = checkRequiredFields(array("fname", "lname", "gender", "dob"), $_POST);
 
 	foreach($dates as $dateField){
 		if(isset($_POST[$dateField])){
@@ -29,7 +42,20 @@ function validateData(){
 	return empty($validationErrors);
 }
 
-function updateMember(){
+function createContact($con){
+	if(!empty(checkRequiredFields(array("fname", "lname", "relationship"), $_POST))){
+		return false;
+	}
+	
+	$contactID = get_guid();
+	DB::executeQuery("INSERT INTO Contacts (ID, FirstName, LastName, Mobile, Landline, Email) VALUES (?, ?, ?, ?, ?, ?)", $con, "ssssss", $contactID, $_POST['fname'], $_POST['lname'], $_POST['mobile'], $_POST['landline'], $_POST['email']);
+
+	DB::executeQuery("INSERT INTO MemberContacts (MemberID, ContactID, RelationshipTypeID) VALUES (?, ?, ?)", $con, "ssi", $_GET['id'], $contactID, $_POST['relationship']);
+
+	return true;
+}
+
+function updateMember($con){
 	if(!validateData()){
 		return false;
 	}
@@ -38,7 +64,7 @@ function updateMember(){
 		$_POST['tetanus'] = null;
 	}
 
-	DB::executeQuery("UPDATE Members SET FirstName = ?, LastName = ?, GenderID = ?, DateOfBirth = ?, MedicalDetails = ?, Allergies = ?, LastTetanus = ?, CanDressWounds = ?, CanAdministerMedication = ? WHERE ID = ?", null, "ssissssiis", $_POST['fname'], $_POST['lname'], $_POST['gender'], $_POST['dob'], $_POST['medical'], $_POST['allergies'], $_POST['tetanus'], isset($_POST['wounds']), isset($_POST['medication']), $_GET['id']);
+	DB::executeQuery("UPDATE Members SET FirstName = ?, LastName = ?, GenderID = ?, DateOfBirth = ?, MedicalDetails = ?, Allergies = ?, LastTetanus = ?, CanDressWounds = ?, CanAdministerMedication = ? WHERE ID = ?", $con, "ssissssiis", $_POST['fname'], $_POST['lname'], $_POST['gender'], $_POST['dob'], $_POST['medical'], $_POST['allergies'], $_POST['tetanus'], isset($_POST['wounds']), isset($_POST['medication']), $_GET['id']);
 	
 	return true;
 }
@@ -47,28 +73,40 @@ if(!isset($_GET['id'])){
 	header("Location: member.php?id=cbe5eee0-e68e-11ed-b2ea-04bf1b5a7502");
 	die();
 } 
-
-$id = $_GET['id'];
-
 $con = DB::connect();
 
-$member = DB::executeQueryForSingle("SELECT * FROM Members WHERE ID = ?", $con, "s", $id);
+$member = DB::executeQueryForSingle("SELECT * FROM Members WHERE ID = ?", $con, "s", $_GET['id']);
 
 if($member == NULL){
 	header("Location: member.php");
 	die();
 }
 
-if(!empty($_POST) && isset($_POST['update'])){
-	if(updateMember()){
-		header("Location: member.php?id=" . $id);
-		die();
+if(!empty($_POST)){
+	if(isset($_POST['update'])){
+		if(updateMember($con)){
+			header("Location: member.php?saved=1&id=" . $_GET['id']);
+			die();
+		}
+	} else if(isset($_POST['contact'])){
+		if($_POST['contactID'] == -1){
+			$result = createContact($con);
+		} else {
+			$result = updateContact($con);
+		}
+
+		if($result){
+			header("Location: member.php?saved=1&id=" . $_GET['id']);
+			die();
+		}
 	}
 }
 
 $genders = DB::executeQuery("SELECT * FROM Genders ORDER BY Name", $con);
-$member['contacts'] = array();
-$member['doctor'] = array();
+$relationships = DB::executeQuery("SELECT * FROM RelationshipTypes ORDER BY SortOrder, Name", $con);
+
+$member['contacts'] = DB::executeQuery("SELECT c.*, r.Name AS Relationship FROM MemberContacts mc INNER JOIN Contacts c ON c.ID = mc.ContactID INNER JOIN RelationshipTypes r ON r.ID = mc.RelationshipTypeID WHERE mc.MemberID = ? ORDER BY r.SortOrder, c.LastName, c.FirstName", $con, "s", $_GET['id']);
+$member['doctor'] = null;
 
 DB::close($con);
 
@@ -91,7 +129,7 @@ require_once('head.php');
 
 		<form method="post">
 			<?php if(!empty($validationErrors)){ ?>
-				<div class="alert alert-danger">Invalid details. Please try again</div>
+			<div class="alert alert-danger">Invalid details. Please try again</div>
 			<?php } ?>
 
 			<div class="card mb-3">
@@ -181,6 +219,17 @@ require_once('head.php');
 			</div>
 		</form>
 
+		<?php if(count($member['contacts']) == 0){ ?>
+		<div class="alert alert-danger">No contacts registered! <button class="btn btn-primary add-contact">Add One Now</button></div>
+		<?php 
+		}
+
+		if($member['doctor'] == null){
+			?>
+		<div class="alert alert-danger">No doctor's surgery on record! <button class="btn btn-primary">Add One Now</button></div>
+		<?php } ?>
+
+
 		<div class="row row-cols-1 row-cols-md-2">
 			<?php 
 					for($i = 0; $i < count($member['contacts']); $i++){ 
@@ -196,47 +245,42 @@ require_once('head.php');
 							<tr>
 								<td>Name</td>
 								<td>
-									<?php echo $contact['firstName'] . " " . $contact['lastName'] ?>
+									<?php echo $contact['FirstName'] . " " . $contact['LastName'] ?>
 								</td>
 							</tr>
 							<tr>
 								<td>Relationship</td>
 								<td>
-									<?php echo $contact['relationship'] ?>
+									<?php echo $contact['Relationship'] ?>
 								</td>
 							</tr>
 							<tr>
 								<td>Mobile</td>
 								<td>
-									<?php echo $contact['mobile'] ?>
+									<?php echo $contact['Mobile'] ?>
 								</td>
 							</tr>
 							<tr>
 								<td>Landline</td>
 								<td>
-									<?php echo $contact['landline'] ?>
+									<?php echo $contact['Landline'] ?>
 								</td>
 							</tr>
 							<tr>
 								<td>Email</td>
-								<td><a href="mailto:<?php echo $contact['email'] ?>">
-										<?php echo $contact['email'] ?>
+								<td><a href="mailto:<?php echo $contact['Email'] ?>">
+										<?php echo $contact['Email'] ?>
 									</a></td>
-							</tr>
-							<tr>
-								<td>Address</td>
-								<td>
-									<?php echo $contact['address']['line1'] ?><br />
-									<?php echo $contact['address']['line2'] ?><br />
-									<?php echo $contact['address']['city'] ?><br />
-									<?php echo $contact['address']['postcode'] ?>
-								</td>
 							</tr>
 						</tbody>
 					</table>
 				</div>
 			</div>
-			<?php } ?>
+			<?php 
+			}
+			
+			if($member['doctor'] != null){
+			?>
 
 			<div class="col">
 				<div class="card">
@@ -265,8 +309,104 @@ require_once('head.php');
 					</table>
 				</div>
 			</div>
+			<?php } ?>
 		</div>
 	</div>
+
+	<form method="post">
+		<div class="modal fade" id="contact" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-hidden="true">
+			<div class="modal-dialog modal-lg">
+				<div class="modal-content">
+					<div class="modal-header">
+						<h2 class="modal-title fs-5" id="contactTitle">Add A Contact</h1>
+							<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+					</div>
+					<div class="modal-body">
+						<div class="row row-cols-1 row-cols-lg-2">
+							<div class="col">
+								<div class="form-floating mb-3">
+									<input type="text" class="form-control" id="cfname" name="fname" placeholder="First Name" required>
+									<label for="fname">First Name</label>
+								</div>
+							</div>
+
+							<div class="col">
+								<div class="form-floating mb-3">
+									<input type="text" class="form-control" id="clname" name="lname" placeholder="Last Name" required>
+									<label for="lname">Last Name</label>
+								</div>
+							</div>
+
+							<div class="col">
+								<div class="form-floating mb-3">
+									<input type="text" class="form-control" id="cmobile" name="mobile" placeholder="Mobile Phone">
+									<label for="mobile">Mobile Phone</label>
+								</div>
+							</div>
+
+							<div class="col">
+								<div class="form-floating mb-3">
+									<input type="text" class="form-control" id="clandline" name="landline" placeholder="Landline">
+									<label for="landline">Landline</label>
+								</div>
+							</div>
+						</div>
+
+						<div class="form-floating mb-3">
+							<input type="text" class="form-control" id="cemail" name="email" placeholder="Email Address">
+							<label for="email">Email Address</label>
+						</div>
+
+						<hr />
+
+						<div class="form-floating">
+							<select id="relationship" name="relationship" class="form-control" required>
+								<option value="">-- Please Select --</option>
+								<?php foreach($relationships as $rel){ ?>
+								<option value="<?php echo $rel['ID'] ?>">
+									<?php echo $rel['Name'] ?>
+								</option>
+								<?php } ?>
+							</select>
+							<label for="relationship">Relationship to Member</label>
+						</div>
+
+						<input type="hidden" name="contactID" id="contactID" />
+					</div>
+					<div class="modal-footer">
+						<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+						<button name="contact" class="btn btn-success">Save</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	</form>
+
+	<script src="js/bootstrap.min.js"></script>
+	<script>
+		function addContact() {
+			showContactModal(-1, "Add A Contact", "", "", "", "", "", "");
+		}
+
+		function showContactModal(id, title, firstName, lastName, mobile, landline, email, relationship) {
+			document.getElementById("contactID").value = id;
+			document.getElementById("contactTitle").innerHTML = title;
+			document.getElementById("cfname").value = firstName;
+			document.getElementById("clname").value = lastName;
+			document.getElementById("cmobile").value = mobile;
+			document.getElementById("clandline").value = landline;
+			document.getElementById("cemail").value = email;
+			document.getElementById("relationship").value = relationship;
+
+			contactModal.show();
+		}
+
+		const contactModal = new bootstrap.Modal(document.getElementById("contact"), {
+			backdrop: 'static'
+		});
+
+		Array.from(document.getElementsByClassName("add-contact")).forEach(el => el.addEventListener("click", addContact));
+	</script>
 </body>
 
 </html>
